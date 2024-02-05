d1191168c87d69ec9190264ebd57372635f8ecb5
Chang <>
2024-02-05 14:15:38 +0800

    Edit build.gradle and comment out those not yet needed

---
+++ src/test/java/ph/edu/cksc/college/advweb/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.cksc.college.advweb.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
